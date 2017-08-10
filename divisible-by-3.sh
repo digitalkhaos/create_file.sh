b7ceb71f079216d383f38a1c76e8da75daf2a120
John DeVito <john.devito@gmail.com>
2017-08-10 14:39:19 -0700

    added script requirments

--- divisible-by-3.sh
+++ divisible-by-3.sh
@@ -1,3 +1,27 @@
+# divisible-by-3
+
+#1) If the script is running, nobody else can run the script.
+#2) The script can only be run as the root user.
+#3) The script will generate 50 files using the touch command.
+#With the following restrictions...
+#If the file number is divisible by 3 or if the file number ends with a 3, it should have the tar.gz extension.
+#Otherwise it has the tgz extension.
+#The filename should look like...
+#delta01.tgz
+#delta02.tgz
+#delta03.tar.gz
+
+
+
+
+
+
+
+
+
+
+
+
 #!/bin/bash
 
 #Check if we're root
