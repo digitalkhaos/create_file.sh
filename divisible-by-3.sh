db3e07342142e46951d7959fef7c22b39311697c
John DeVito <john.devito@gmail.com>
2017-08-11 13:54:16 -0700

    added file creating plus cheese

--- divisible-by-3.sh
+++ divisible-by-3.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # divisible-by-3
 
 #1) If the script is running, nobody else can run the script.
@@ -11,21 +13,8 @@
 #delta02.tgz
 #delta03.tar.gz
 
-
-
-
-
-
-
-
-
-
-
-
-#!/bin/bash
-
 #Check if we're root
-if [[ $EUID -ne 0 ]]; then
+if [ $EUID -ne 0 ]; then
   echo "You must be a root user" 2>&1
   exit 1
 fi
@@ -38,4 +27,26 @@ for pid in $(pidof -x divisible-by-3.sh); do
     fi
 done
 
+#Create 50 files
+for i in {1..50}; do
 
+	if [ $i==13 ]; then
+		touch "/home/john/code/scripts/script_practice/tmp/delta13.tar.gz" 
+		continue
+	fi
+	if [ $i==23 ]; then
+		touch "/home/john/code/scripts/script_practice/tmp/delta23.tar.gz"
+		continue
+	fi
+	if [ $i==43 ]; then
+		touch "/home/john/code/scripts/script_practice/tmp/delta43.tar.gz"
+		continue
+	fi
+	if [ $(($i%3)) == 0 ]; then
+		touch "/home/john/code/scripts/script_practice/tmp/delta$i.tar.gz"
+	else
+		touch "/home/john/code/scripts/script_practice/tmp/delta$i.tgz"
+	fi
+done
+
+echo "All files created successfully!"
